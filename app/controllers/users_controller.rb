4f3b5a7a1397b4469f91379fb1da006379ba7785
Yors <jatbribiesca@gmail.com>
2020-12-07 17:11:08 -0600

    add form to new.html.erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,10 @@
-class NewController < ApplicationController
+class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
   end
 
   def show
