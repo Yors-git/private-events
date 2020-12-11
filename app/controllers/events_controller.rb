823be38d5a735ffeb22797d71c81ba63609d7da2
Yors <jatbribiesca@gmail.com>
2020-12-11 12:22:46 -0600

    divide events in user show and event index into past and upcomming

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,6 +2,8 @@ class EventsController < ApplicationController
   include ApplicationHelper
   def index
     @events = Event.all
+    @past_events = Event.past
+    @future_events = Event.future
   end
 
   def new
--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,7 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User'
   has_many :attendances, dependent: :destroy
   has_many :attendee_users, through: :attendances, source: :user
+
+  scope :past, -> { where('date_of_event<?', Time.now) }
+  scope :future, -> { where('date_of_event>?', Time.now) }
 end
